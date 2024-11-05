4a83979d1691096d55f7a87315a18d98cb3bbfe7
Kooreis <kooreis.official@gmail.com>
2024-11-05 15:18:46 -0600

    This is the BFS class that implements the Breadth-First Search algorithm to solve the sliding puzzle. It maintains a queue of boards to visit and a set of visited boards. It iteratively visits each board in the queue, checks if it is the goal state, and if not, adds its neighbors to the queue.

--- solution.java
+++ solution.java
@@ -1,13 +1,35 @@
-import java.util.*;
+class BFS {
+    private Board initial;
+    private Board goal;
 
-public class Main {
-    public static void main(String[] args) {
-        int[][] initialBoard = {{1, 2, 3}, {4, 0, 5}, {7, 8, 6}};
-        int[][] goalBoard = {{1, 2, 3}, {4, 5, 0}, {7, 8, 6}};
-        Board initial = new Board(initialBoard);
-        Board goal = new Board(goalBoard);
+    public BFS(Board initial, Board goal) {
+        this.initial = initial;
+        this.goal = goal;
+    }
 
-        BFS bfs = new BFS(initial, goal);
-        bfs.solve();
+    public void solve() {
+        Queue<Board> queue = new LinkedList<>();
+        Set<Board> visited = new HashSet<>();
+
+        queue.add(initial);
+        visited.add(initial);
+
+        while (!queue.isEmpty()) {
+            Board current = queue.poll();
+
+            if (current.equals(goal)) {
+                current.printPath();
+                return;
+            }
+
+            for (Board next : current.neighbors()) {
+                if (!visited.contains(next)) {
+                    queue.add(next);
+                    visited.add(next);
+                }
+            }
+        }
+
+        System.out.println("No solution found");
     }
 }
