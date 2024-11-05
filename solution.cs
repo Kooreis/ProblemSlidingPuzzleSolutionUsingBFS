2038008d6f9cea2b8c5f957cf2b37cde40c553e4
Kooreis <kooreis.official@gmail.com>
2024-11-05 15:18:46 -0600

    This is the Puzzle class with its constructor. It takes initial and goal states as parameters and stores them in private variables. It also calculates the size of the puzzle.

--- solution.cs
+++ solution.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public class Program
+public class Puzzle
 {
-    static void Main(string[] args)
+    private int[,] initial;
+    private int[,] goal;
+    private int size;
+
+    public Puzzle(int[,] initial, int[,] goal)
     {
-        int[,] initial = { { 1, 2, 3 }, { 5, 0, 6 }, { 7, 8, 4 } };
-        int[,] goal = { { 1, 2, 3 }, { 5, 8, 6 }, { 0, 7, 4 } };
-        Puzzle puzzle = new Puzzle(initial, goal);
-        puzzle.Solve();
+        this.initial = initial;
+        this.goal = goal;
+        this.size = initial.GetLength(0);
     }
 }
